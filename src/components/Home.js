e0be5a1db91187c95f64c6f4c262c761e4f2fcca
kevkouadio <kevkouadio@gmail.com>
2022-02-08 23:13:15 -0500

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import "./Home.css"
+
+function Home() {
+    return(
+        <div className="container">
+            <div className="align-self-center" id="home-div">
+                <h1>Welcome To Good Cook</h1>
+                    <p>Come and eat well with our delicious & healthy foods.</p>
+                <button id="home-btn">Reservation</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
